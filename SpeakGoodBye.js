04503a87a1217b822275c9502dfaac65ea2f5cb8
kellypeanut <102782399+kellypeanut@users.noreply.github.com>
2022-04-29 10:31:26 +0800

    Create SpeakGoodBye.js

---
+++ SpeakGoodBye.js
@@ -0,0 +1,10 @@
+(function (window) {
+  var byeSpeaker = {
+    speak
+  }
+var speakWord = "Good Bye";
+function speak(name) {
+  console.log(speakWord + " " + name);
+}
+window.byeSpeaker = byeSpeaker;
+})(window)
